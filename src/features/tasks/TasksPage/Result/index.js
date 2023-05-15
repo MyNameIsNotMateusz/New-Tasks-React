34a667f8e4294adafea091d7c319c6cc43c34c6b
Mateusz Otorowski <mati1otorowski@gmail.com>
2023-05-15 17:03:28 +0200

    i removed map

--- src/features/tasks/TasksPage/Result/index.js
+++ src/features/tasks/TasksPage/Result/index.js
@@ -21,9 +21,46 @@ const Result = ({ tasks }) => {
 
   return (
     <div>
+      {(() => {
+        const result = [];
+        for (let i = 0; i < tasks.length; i++) {
+          const task = tasks[i];
+          if (!task.isHidden) {
+            result.push(
+              <ResultWrapper
+                onClick={() => handleCompleteTask(task.id)}
+                key={task.id}
+              >
+                <span
+                  style={{
+                    textDecoration: task.completed ? "line-through" : "none"
+                  }}
+                >
+                  {task.text}
+                </span>
+                <DivWrapper>
+                  <SpanWrapper>
+                    <Link
+                      to={`/tasks/${task.id}`}
+                      onClick={handleLinkClick}
+                    >
+                      🕵
+                    </Link>
+                  </SpanWrapper>
+                  <SpanWrapper onClick={() => handleRemoveTask(task.id)}>
+                    🗑️
+                  </SpanWrapper>
+                </DivWrapper>
+              </ResultWrapper>
+            );
+          }
+        }
+        return result;
+      })()}
     </div>
   );
-
+  
+  
 };
 
 export default Result;
