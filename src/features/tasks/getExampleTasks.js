98b480376b0f596b767eebc95a3c693fd61b2e9a
Mateusz Otorowski <mati1otorowski@gmail.com>
2023-05-15 18:13:46 +0200

    i fixed problem with get example tasks

--- src/features/tasks/getExampleTasks.js
+++ src/features/tasks/getExampleTasks.js
@@ -1,5 +1,5 @@
 export const getExampleTasks = async () => {
-    const response = await fetch("/exampleTasks.json")
+    const response = await fetch("/newTasks-react/exampleTasks.json")
 
     if(!response.ok) {
         new Error(response.statusText);
